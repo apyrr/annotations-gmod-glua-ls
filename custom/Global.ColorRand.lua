3084cd17d82df71457fdeae01525a8c6a4cdad85
apyrr <116293161+apyrr@users.noreply.github.com>
2026-04-23 20:10:15 +0300

    Create Global.ColorRand.lua

---
+++ custom/Global.ColorRand.lua
@@ -0,0 +1,7 @@
+---Creates a [Color](https://wiki.facepunch.com/gmod/Color) with randomized red, green, and blue components. If the alpha argument is true, alpha will also be randomized.
+---@realm shared
+---@realm menu
+---@source https://wiki.facepunch.com/gmod/Global.ColorRand
+---@param a? boolean Should alpha be randomized.
+---@return Color # The created Color.
+function _G.ColorRand(a) end
